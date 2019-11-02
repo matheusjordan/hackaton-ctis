ce6f4aa559d76713c5442bf68e39f5f68026ba83
Matheus Jordan <matheusj1599@gmail.com>
2019-11-02 00:07:57 -0300

    Criado formulario de categoria

--- src/app/components/category/add-category/add-category.component.ts
+++ src/app/components/category/add-category/add-category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-category',
@@ -8,13 +9,24 @@ import { ModalController } from '@ionic/angular';
 })
 export class AddCategoryComponent implements OnInit {
 
+  categoryForm: FormGroup;
+
   constructor(
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private fb: FormBuilder
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createForm();
+  }
 
   closeModal() {
     this.modalCtrl.dismiss();
+  }
+
+  private createForm() {
+    this.categoryForm = this.fb.group({
+      name: [null, [Validators.required]]
+    });
   }
 }
