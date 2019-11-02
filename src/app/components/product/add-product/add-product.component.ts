9ebc9ffe16252cde01e5ce028a1486d089d66510
Matheus Jordan <matheusj1599@gmail.com>
2019-11-01 23:52:25 -0300

    Criado formulario de produto

--- src/app/components/product/add-product/add-product.component.ts
+++ src/app/components/product/add-product/add-product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -8,13 +9,28 @@ import { ModalController } from '@ionic/angular';
 })
 export class AddProductComponent implements OnInit {
 
+  productForm: FormGroup;
+
   constructor(
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private fb: FormBuilder
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createForm();
+  }
 
   closeModal() {
     this.modalCtrl.dismiss();
   }
+
+  private createForm() {
+    this.productForm = this.fb.group({
+      name: [null, [Validators.required]],
+      value: [null, [Validators.required]],
+      category: [null, [Validators.required]],
+      description: [null, [Validators.required]]
+    });
+  }
+
 }
